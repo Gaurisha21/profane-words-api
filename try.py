74cff1e8d298eb2d8b3ed297954cab1914a967a4
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-09-12 16:08:50 +0530

    files for local host

---
+++ try.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+from profanity_filter import ProfanityFilter
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''Safe forum'''
+
+@app.route('/predict/<string:txt>',methods=['GET'])
+def predict(txt):
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    # str_features = request.form.values()
+    pf = ProfanityFilter()
+    # postStr = list(str_features)
+    output = pf.censor(txt)
+
+    return jsonify({'txt': output})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
